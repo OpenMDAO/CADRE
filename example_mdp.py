1e1d7bf8baa5e6b2565de2a9fcee374556008cde
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2015-09-30 11:12:20 -0400

    ram

--- example_mdp.py
+++ example_mdp.py
@@ -90,15 +90,15 @@ for j in range(npts):
     recording_includes_options.append('pt%s.ConS0' % str(j))
     recording_includes_options.append('pt%s.ConS1' % str(j))
 
-# from openmdao.recorders import DumpRecorder
-# rec = DumpRecorder(out='data.dmp')
-# model.driver.add_recorder(rec)
-# rec.options['includes'] = recording_includes_options
-
-from openmdao.recorders import SqliteRecorder
-rec = SqliteRecorder(out='data.sql')
+from openmdao.recorders import DumpRecorder
+rec = DumpRecorder(out='data.dmp')
 model.driver.add_recorder(rec)
 rec.options['includes'] = recording_includes_options
+
+#from openmdao.recorders import SqliteRecorder
+#rec = SqliteRecorder(out='data.sql')
+#model.driver.add_recorder(rec)
+#rec.options['includes'] = recording_includes_options
 
 model.setup()
 model.run()
