1fa8e07ff9431a4854ae3f5cb002630ccc4a1177
swryan <stephen.w.ryan@gmail.com>
2018-07-13 20:15:03 -0400

    fix manifest

--- setup.py
+++ setup.py
@@ -5,26 +5,24 @@
 from setuptools import setup
 
 kwargs = {
+    'name': 'CADRE',
+    'description': 'Implementation of the CADRE CubeSat design problem for OpenMDAO 2.x',
+    'license': 'Apache 2.0',
     'author': 'Kenneth T. Moore',
     'author_email': 'kenneth.t.moore-1@nasa.gov',
+    'maintainer': 'Kenneth T. Moore',
+    'maintainer_email': 'kenneth.t.moore-1@nasa.gov',
     'classifiers': [
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering'
     ],
-    'description': 'Implementation of the CADRE CubeSat design problem for OpenMDAO 2.x',
-    'download_url': 'http://github.com/OpenMDAO/CADRE.git',
-    'include_package_data': True,
-    'install_requires': ['openmdao>=2.3'],
     'keywords': ['openmdao', 'CADRE'],
-    'license': 'Apache 2.0',
-    'maintainer': 'Kenneth T. Moore',
-    'maintainer_email': 'kenneth.t.moore-1@nasa.gov',
-    'license': 'Apache License, Version 2.0',
-    'name': 'CADRE',
-    'package_data': {'CADRE': ['test/data/*.pkl']},
-    'packages': ['CADRE', 'CADRE.test'],
-    'include_package_data': True,
     'url': 'http://github.com/OpenMDAO/CADRE.git',
+    'download_url': 'http://github.com/OpenMDAO/CADRE.git',
+    'install_requires': ['openmdao>=2.3'],
+    'packages': ['CADRE', 'CADRE.test'],
+    'package_data': {'CADRE': ['data/*.pkl', 'test/*.pkl']},
+    'include_package_data': True,
     'version': '0.2',
     'zip_safe': False
 }
